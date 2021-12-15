37ea50c1895239831356b18953e03b24f53b4903
lee <o5046821854@gmail.com>
2021-12-16 02:52:12 +0900

    211216

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 const url = require('url');
 const session = require('express-session')
 const MySQLStore = require('express-mysql-session')(session);
-const { uploadFile, getFileStream, deleteFile } = require('./s3.js')
+//const { uploadFile, getFileStream, deleteFile } = require('./s3.js')
 const data = fs.readFileSync('./database.json')
 const conf = JSON.parse(data)
 const path = require('path')
@@ -19,6 +19,7 @@ const { networkInterfaces } = require('os');
 const s3 = new aws.S3()
 
 
+app.set('views', './views/templates');
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -26,27 +27,12 @@ app.use(express.json())
 const Dirpath = path.join(__filename, "../")
 app.use(express.static(Dirpath))
 
+
 const PORT = 3000 || process.env.PORT
-
-//사용자 로그인 여부 미들웨어
-const auth = function(req, res, next) {
-    if (!req.session.user)
-        res.send("<script>alert('로그인 후 이용가능합니다');window.location.href='/login';</script>")
-    else
-        next();
-}
-
-//사용자 권한 판단 미들웨어
-const authAdmin = function(req, res, next) {
-    if (req.session.user.authority <= 3)
-        res.send(`res.send("<script>alert('접근 권한이 없습니다');window.location.href="javascript:window.history.back();";</script>")`);
-    else
-        next();
-}
 
 //이미지 파일 저장 multer 미들웨어
 const upload = multer({
-    dest: "logo"
+    dest: "image"
 });
 
 //mysql 연결 정보
@@ -56,6 +42,15 @@ const db = mysql.createConnection({
     password: conf.password,
     database: conf.database
 })
+
+
+//db에 연결
+db.connect((err) => {
+    if (err)
+        throw err;
+    console.log("connected")
+})
+
 
 //유저 로그인 세션 정보 저장용
 app.use(
@@ -67,32 +62,87 @@ app.use(
 )
 
 
-//db에 연결
-db.connect((err) => {
-    if (err)
-        throw err;
-    console.log("connected")
-})
+//사용자 로그인 여부 미들웨어
+const auth = function(req, res, next) {
+    if (!req.session.user)
+        res.send("<script>alert('로그인 후 이용가능합니다');window.location.href='/login';</script>")
+    else
+        next();
+}
+
+//사용자 권한 판단 미들웨어
+const authAdmin = function(req, res, next) {
+    if (req.session.user.authority < 2)
+        res.send(`res.send("<script>alert('접근 권한이 없습니다');window.location.href="javascript:window.history.back();";</script>")`);
+    else
+        next();
+}
+
 
 //인덱스 페이지
 app.get('', (req, res) => {
     if (req.session.user) //로그인 여부에 따라 페이지 로드
-        res.redirect("/getOne")
+        res.redirect("/search")
     else
         res.redirect("/login")
 })
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//회원가입 페이지 (get)
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs')
+})
+
+//회원가입 페이지 (post)
+app.post('/signup', (req, res) => {
+    res.redirect('login')
+})
+
+//공구 페이지 (get)
+app.get('/purchase', (req, res) => {
+
+    res.render('purchase.ejs')
+})
+
+
+//마이 페이지 (get)
+app.get('/myPage', (req, res) => {
+
+    res.render('myPage.ejs')
+})
+
+//후기 페이지 (get)
+app.get('/review', (req, res) => {
+
+        res.render('review.ejs')
+    })
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//로그인 페이지 (get)
+app.get('/login', (req, res) => {
+
+    res.render('login.ejs')
+
+})
+
 
 //로그인 로직
 const login = async function(id, pw) {
     let result2 = "";
     let sql =
-        `select * from CLUB_OLD where admin_id = "${id}"`
+        `select * from USER where id = "${id}"`
     const result = await db.promise().query(sql)
     const textRow = JSON.parse(JSON.stringify(result[0]))
     if (!textRow[0]) {
         result2 = "No id"
     } else {
-        const isMatch = await bcrypt.compare(pw, textRow[0].admin_pw);
+        var isMatch = true;
+        if (pw !== textRow[0].password)
+            isMatch = false;
         if (isMatch)
             result2 = textRow[0];
         else
@@ -103,23 +153,19 @@ const login = async function(id, pw) {
     })
 }
 
-//로그인 페이지 (get)
-app.get('/login', (req, res) => {
-    res.render('login.ejs');
-})
 
 //로그인 페이지 (post)
 app.post('/login', async(req, res) => {
     var id = req.body.id;
     var pw = req.body.pwd;
     const result = await login(id, pw);
-    if (result.cid) { //해당 id/pw로 이루어진 계정 존재 시
+    if (result.id) { //해당 id/pw로 이루어진 계정 존재 시
         req.session.user = result
         res.cookie('cnt', 0)
         if (result.authority <= 3) { //권한에 따라 페이지 로드 
-            res.redirect("/getOne")
+            res.redirect("/search")
         } else {
-            res.redirect('/getAll')
+            res.redirect('/search')
         }
     } else { //로그인 횟수 저장 (TBD)
         if (!req.cookies.cnt) {
@@ -127,7 +173,7 @@ app.post('/login', async(req, res) => {
         } else {
             res.cookie('cnt', Number(req.cookies.cnt) + 1)
         }
-        res.send(`<script> alert("그런 계정은 없습니다"); window.location.href='/login'; </script>`)
+        res.send(`<script> alert("다시 로그인해주세요"); window.location.href='/login'; </script>`)
     }
 })
 
@@ -135,20 +181,21 @@ app.post('/login', async(req, res) => {
 //로그아웃 페이지 (get)
 app.get('/logout', (req, res) => {
     req.session.user = undefined;
-    //로그아웃 : destroy 메소드
     res.send("<script>alert(`로그아웃되었습니다.\n다시 로그인 해주세요`);window.location.href='/login';</script>")
 })
 
 
 //일반사용자 조회 페이지
-app.get('/getOne', auth, (req, res) => {
-    let sql = `select * from CLUB_OLD WHERE cid = ${req.session.user.cid}`
+app.get('/search', (req, res) => {
+    let sql = `select * from MASK`
+
     db.query(sql, async(err, result) => {
         if (err)
             console.log(err);
         req.session.user = result[0]
-        res.render('myInfo.ejs', {
-            data: JSON.parse(JSON.stringify(result[0]))
+        res.render('search.ejs', {
+            dataArr: result,
+            login: req.session.user
         })
     })
 })
@@ -163,115 +210,12 @@ app.get('/updateData', auth, (req, res) => {
 //회원정보 수정 페이지 (post)
 app.post('/updateData', (req, res) => {
     const data = req.body
-    let sql = `update CLUB_OLD SET cname = "${data.cname}", category1= "${data.category1}", category2 = "${data.category2}", category3 = "${data.category3}", campus = "${data.campus}", estab_year = "${data.estab_year}",intro_text = "${data.intro_text}", intro_sentence = "${data.intro_sentence}", activity_info = "${data.activity_info}", meeting_time = "${data.meeting_time}", activity_location = "${data.activity_location}", activity_num = "${data.acticity_num}", recruit_season = "${data.recruit_season}", activity_period = "${data.activity_period}", recruit_process = "${data.recruit_process}", recruit_num = "${data.recruit_num}", recruit_site = "${data.recruit_site}", president_name = "${data.president_name}", president_contact = "${data.president_contact}", emergency_contact = "${data.emergency_contact}", website_link = "${data.website_link}", website_link2 = "${data.website_link2}"  WHERE cid = ${data.cid} `
+    let sql = `UPDATE USER SET ADDRESS = "${data.address}", NICKNAME= "${data.nickname}", kf = "${data.kf}", color = "${data.color}", shape = "${data.shape}", strap = "${data.strap}", kfnum = "${data.kfnum}", colornum = "${data.colornum}", shapenum = "${data.shapenum}", strapnum = "${data.strapnum}" `
     db.query(sql, async(err, result) => {
         if (err)
             console.log(err);
         res.redirect('/getOne')
     })
-})
-
-
-app.post('/updated', (req, res) => {
-    if (req.body.updatedData !== undefined) {
-        req.body.updatedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET upt = 1 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.sharedData !== undefined) {
-        req.body.sharedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET share = 1 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.unupdatedData !== undefined) {
-        req.body.unupdatedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET upt = 0 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.unsharedData !== undefined) {
-        req.body.unsharedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET share = 0 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-})
-
-//관리자 용 동아리 조회 페이지 
-app.get('/getAll', auth, authAdmin, (req, res) => {
-    res.render('master.ejs')
-})
-
-//관리자 용 동아리 조회 페이지 - ajax 요청 처리 로직
-app.get('/getData', (req, res) => {
-    let sql = "SELECT a.* ,b.share ,b.upt FROM CLUB_OLD AS a INNER JOIN SHARE AS b ON a.cid = b.cid"
-    db.query(sql, async(err, data) => {
-        if (err)
-            throw err;
-        res.send((JSON.stringify(data)))
-    })
-})
-
-//관리자 조회 페이지 => 동아리 권한 설정 기능
-app.post('/updated', (req, res) => {
-    if (req.body.updatedData !== undefined) {
-        req.body.updatedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET upt = 1 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.sharedData !== undefined) {
-        req.body.sharedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET share = 1 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.unupdatedData !== undefined) {
-        req.body.unupdatedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET upt = 0 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
-    if (req.body.unsharedData !== undefined) {
-        req.body.unsharedData.forEach((e) => {
-            let sql =
-                `UPDATE SHARE SET share = 0 WHERE CID = "${Number(e)}"`
-            db.query(sql, async(err, result) => {
-                if (err)
-                    throw err;
-            })
-        })
-    }
 })
 
 
@@ -292,7 +236,7 @@ app.post('/changePWD', (req, res) => {
             if (err) {
                 res.send("<script>alert('에러발생');window.location.href='/changePWD';</script>")
             }
-            let sql = `update CLUB_OLD set admin_pw = "${hashedPWD}" WHERE cid = ${req.session.user.cid}`
+            let sql = `update CLUB_OLD set pw = "${hashedPWD}" WHEREE cid = ${req.session.user.cid}`
             db.query(sql, async(err, result) => {
                 if (err)
                     res.send("<script>alert('에러발생');window.location.href='/changePWD';</script>")
@@ -307,10 +251,3 @@ app.post('/changePWD', (req, res) => {
 app.listen(3000, () => {
     console.log("server is on " + PORT)
 })
-
-
-
-/**
-    TODO : 임시저장 => 각 상황에 따라 상태관리 => 임시저장버튼
-    동아리 게시판 / 관리자, 일반사용자 기능 (공지페이지, 마스터만 글을 쓰고 수정할 수 있도록)
-**/
